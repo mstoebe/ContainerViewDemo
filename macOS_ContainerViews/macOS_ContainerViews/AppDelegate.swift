24f17d2dec4b5c471d907fc459b56ea3a752c5b8
Markus Stöbe <markus@stoebe.me>
2018-05-26 13:56:28 +0200

    added macOS-Demo

---
+++ macOS_ContainerViews/macOS_ContainerViews/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS_ContainerViews
+//
+//  Created by Markus Stöbe on 26.05.18.
+//  Copyright © 2018 Markus Stöbe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ macOS_ContainerViews/macOS_ContainerViews/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS_ContainerViews
+//
+//  Created by Markus Stöbe on 26.05.18.
+//  Copyright © 2018 Markus Stöbe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
